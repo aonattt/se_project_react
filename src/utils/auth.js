e9bb64e1ee023ae726f78f65ff24b9bd0c62547c
aonattt <aonattt@gmail.com>
2026-09-12 04:08:47 +0900

    Update frontend configuration and homepage for cloud deployment

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,9 @@
 import { checkResponse } from "./api";
 
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.aonat-wtwr.strangled.net"
+    : "http://localhost:3001";
 
 export const register = (name, avatar, email, password) => {
   return fetch(`${BASE_URL}/signup`, {
